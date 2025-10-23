a67485e3f13d2fe8080585ffd48cbb25e230c727
JustinChenMedia <101614596@student.swin.edu.au>
2025-10-23 17:50:15 +1100

    Product and Product Catalogue Implementation

--- YourLocalShop/src/Main.java
+++ YourLocalShop/src/Main.java
@@ -1,7 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        ProductCatalog catalog = new ProductCatalog();
+        catalog.loadProducts("src/data/products.json");
+        catalog.displayAll();
     }
 }
